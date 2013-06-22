926a7a8620bc98c4b05edce2e86c975f6055b3ab
AAlvz <alfonso.alvz@gmail.com>
2013-06-22 18:11:00 -0500

    script to evaluate my bash commands

---
+++ historyCommands.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+from collections import Counter
+cnt = Counter()
+for line in open('/home/alfonso/.bash_history', 'r'):
+    cnt[line] += 1
+for k, v in cnt.most_common()[:-10:-1]:
+    print k + " "+ str((v*100.00/sum(cnt.values())))+" %\n"
+#unidict = {k.decode('utf8'): v for k, v in cnt.items()}
